7c2124ab1f2baedbdb4071c19181d67d4b07a74b
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-07-05 07:53:21 +0700

    server-side rendering (SSR)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
-    return <h1>Landing page 1</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+    return currentUser ? <h1>You are sign in</h1> : <h1>Your are not sign in</h1>
 };
+
+LandingPage.getInitialProps = async (context) => {
+    console.log("INDEX on SERVER");
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+    return data;
+};
+
+export default LandingPage;
