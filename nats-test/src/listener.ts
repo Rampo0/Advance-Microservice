ad77c87e09607e7cc5364f0665265d7870f7036f
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-08-05 22:06:17 +0700

    event-bus publisher listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -13,30 +14,12 @@ stan.on("connect", () => {
   stan.on('close', () => {
       console.log('NATS connection closed!!');
       process.exit();
-  })
-
-  const options = stan.subscriptionOptions()
-  .setManualAckMode(true)
-  .setDeliverAllAvailable()
-  .setDurableName('accounting-service');
-  
-  const subscription = stan.subscribe(
-    "ticket:created",  
-    "queue-group-name",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
   });
-});
 
+  new TicketCreatedListener(stan).listen();
+
+});
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
