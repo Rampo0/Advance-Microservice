fe3d9b5ed1cc9460813525ab0d251262e508bd36
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-08-06 01:10:22 +0700

    accessing nats with singleton

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
-
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
 
@@ -13,6 +13,12 @@ const start = async () => {
     }
  
     try{
+        await natsWrapper.connect(
+            'ticketing',
+            'akhsdaduwqecb',
+            'http://nats-clusterip-service:4222'
+        )
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser : true,
             useUnifiedTopology : true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,9 @@
 import express, { Request , Response } from 'express';
-import { requireAuth , validateRequest } from '@rampooticketing/common'
+import { requireAuth , validateRequest } from '@rampooticketing/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,8 +25,15 @@ router.post('/api/tickets', requireAuth, [
         price,
         userId : req.currentUser!.id
     });
-
+    
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId
+    });
+
     res.status(201).send(ticket);
 });
 
