43026083cb4182d34627d0dc49a5f7e6c8656548
rampo0 <36811859+Rampo0@users.noreply.github.com>
2020-11-11 18:44:45 +0700

    add startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { app } from './app';
 
 const start = async () => {
 
+    console.log("Starting up auth service ...");
+    
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY is not defined.");
     }
