3515fa292618a64f7a10fc62497fbb7f2a355a70
rampo0 <36811859+Rampo0@users.noreply.github.com>
2020-11-12 18:33:38 +0700

    remove console

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 
 const start = async () => {
 
-    console.log("Starting up auth service ...");
+    // console.log("Starting up auth service ...");
     
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY is not defined.");
