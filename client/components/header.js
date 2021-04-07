538cf609ecad8b66b898f84dd04cbf50268e5040
Ammar Dwi Anwari <36811859+Rampo0@users.noreply.github.com>
2021-04-08 02:14:48 +0700

    change logo

--- client/components/header.js
+++ client/components/header.js
@@ -19,7 +19,7 @@ export default ({ currentUser }) => {
         });
     return <nav className="navbar navbar-light bg-light">
         <Link href="/">
-            <a className="navbar-brand">GitTix</a>
+            <a className="navbar-brand">Logo</a>
         </Link>
         <div className="d-flex justify-content-end">
             <ul className="nav d-flex align-items-center">
