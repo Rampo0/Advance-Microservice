a0e0140a75ea44c7734d1661566295b9e4c1f19f
Ammar Dwi Anwari <>
2022-09-08 14:52:11 +0700

    versioning image

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# docker for mac
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.41.0/deploy/static/provider/cloud/deploy.yaml
+# docker for mac note: always update this url
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.41.0/deploy/static/provider/cloud/deploy.yaml
 
 kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
 kubectl create secret generic stripe-secret --from-literal=STRIPE_KEY=sk_test_In8WodA92bfRdCb4iyLAzy0N00DnC8iL6f
